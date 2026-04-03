edb36a3de255f1b885e3f5ec5c8db4304f6d29fd
efezino <enduranceidisi13@gmail.com>
2026-04-03 21:40:14 +0100

    refactor: consolidate ModalWrapper props into a single object and remove unused baseUrl from tsconfig

--- src/components/ModalWrapper.vue
+++ src/components/ModalWrapper.vue
@@ -1,25 +1,25 @@
 <template>
-  <dialog ref="modal" class="modal" :class="classes" @click="handleDialogClick" @close="onClose">
+  <dialog ref="modal" class="modal" :class="props.classes" @click="handleDialogClick" @close="props.onClose">
     <div class="px-5 w-full h-full space-y-5 pb-5">
       <slot />
 
-      <footer class="w-full flex justify-end p-3" v-if="showCancel || showConfirm">
+      <footer class="w-full flex justify-end p-3" v-if="props.showCancel || props.showConfirm">
         <button
           type="button"
-          v-if="showCancel"
+          v-if="props.showCancel"
           @click="closeModal"
           class="capitalize border border-purple-500 px-5 py-2 rounded-md text-purple-500"
         >
-          {{ cancelText }}
+          {{ props.cancelText }}
         </button>
 
         <button
           type="button"
-          v-if="showConfirm"
+          v-if="props.showConfirm"
           @click.prevent="confirm"
           class="capitalize bg-purple-500 px-5 py-2 rounded-md text-white"
         >
-          {{ confirmText }}
+          {{ props.confirmText }}
         </button>
       </footer>
     </div>
@@ -31,8 +31,7 @@ import { ref } from 'vue'
 
 const modal = ref<HTMLDialogElement>()
 
-const { confirmText, cancelText, showConfirm, showCancel, classes, allowClickOutside, onClose } =
-  withDefaults(
+const props = withDefaults(
     defineProps<{
       confirmText?: string
       cancelText?: string
@@ -81,7 +80,7 @@ defineExpose({
 function handleDialogClick(event: MouseEvent) {
   const target = event.target as HTMLDialogElement
 
-  if (!allowClickOutside) return
+  if (!props.allowClickOutside) return
 
   if (target && target.classList.contains('modal')) {
     closeModal()
